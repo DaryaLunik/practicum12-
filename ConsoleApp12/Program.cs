b7c7286cce30663cc1268fb875d92c475f9e6733
DaryaLunik <dawa23112007@gmail.com>
2024-11-19 14:39:47 +0200

    Add files via upload

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        //public static double Funk(int x)
+        //{
+        //    return (Math.Sqrt(x) + x) / 2;
+        //}
+        static void Main(string[] args)
+        {
+            //Задача 1.Вычислить значение выражения, определив и использовав дополнительный метод.
+            //double x = Funk(5) + Funk(12) + Funk(19);
+            //Console.WriteLine($"x={x}");
+            //Console.ReadKey();
+        }
+    }
+}
